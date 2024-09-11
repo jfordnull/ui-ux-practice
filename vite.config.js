84bae7f6bda0743b6b4169968fc78441a1af3d53
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-09-10 23:34:15 -0500

    First commit! Nice

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+export default {
+	root: 'src/',
+	publicDir: '../static/',
+	server:
+	{
+		host: true,	
+		open: true
+	},
+	build:
+	{
+		outDir: '../dist',	
+		emptyOutDir: true,
+		sourcemap: true
+	
+	}
+}
